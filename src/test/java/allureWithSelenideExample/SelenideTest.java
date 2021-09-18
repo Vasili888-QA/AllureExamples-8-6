8bb8ca8a7e59322dd462e106db8986bbee0b96ff
Vasili888-QA <vasil88888@inbox.ru>
2021-09-18 18:40:11 +0300

    Add simple test with Selenide Listener and Allure

---
+++ src/test/java/allureWithSelenideExample/SelenideTest.java
@@ -0,0 +1,39 @@
+package allureWithSelenideExample;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+@Epic("Epic annotation - Simple example with Allure by Selenide Listener")
+@Feature("Feature annotation - Allure with Selenide Listener")
+@DisplayName("DisplayName - Simple example with Allure by Selenide Listener")
+public class SelenideTest {
+
+    @Test
+    @Story("Story annotation - Github test")
+    @DisplayName("DisplayName - Check issue name on Github")
+    public void testGithub() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).click();
+        $(byText("68")).should(Condition.visible);
+    }
+}
