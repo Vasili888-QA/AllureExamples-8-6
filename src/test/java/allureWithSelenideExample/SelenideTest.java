440ea3b89e3e6f6c9ac3478c3a16d2c972493390
Vasili888-QA <vasil88888@inbox.ru>
2021-10-03 16:42:20 +0300

    Fix locator for broken step

--- src/test/java/allureWithSelenideExample/SelenideTest.java
+++ src/test/java/allureWithSelenideExample/SelenideTest.java
@@ -10,9 +10,9 @@ import lambdaAllureStepsExample.TestBase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.partialLinkText;
 
@@ -35,6 +35,6 @@ public class SelenideTest extends TestBase {
 
         $(linkText("eroshenkoam/allure-example")).click();
         $(partialLinkText("Issues")).click();
-        $(byText("74")).should(Condition.visible);
+        $x("//span[@class='opened-by']").shouldHave(text("74"));
     }
 }
--- src/test/java/lambdaAllureStepsExample/LambdaStepsTest.java
+++ src/test/java/lambdaAllureStepsExample/LambdaStepsTest.java
@@ -7,9 +7,9 @@ import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.partialLinkText;
@@ -40,7 +40,7 @@ public class LambdaStepsTest extends TestBase {
         step("Click on tab Issues", () -> $(partialLinkText("Issues")).click());
 
         step("Check Issue with number " + ISSUE_NUMBER, () -> {
-            $(byText("#" + ISSUE_NUMBER)).should(Condition.visible);
+            $x("//span[@class='opened-by']").shouldHave(text("74"));
         });
     }
 }
